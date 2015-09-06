7320654dd2d0eb9979ef6c1c0a8603531c8f1272
Madeline Schimenti <madelinelise@gmail.com>
2015-09-05 21:00:59 -0400

    Fig. 4.2
    Created GradeBookTest.cs.
    Added constructor and called DisplayMessage()

---
+++ GradeBookTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+//Create a GradeBook object and call its DisplayMessage method.
+public class GradeBookTest
+{
+    // Main method begins program execution
+	public static void Main( string[] args )
+	{
+        // Create a GradeBook object and assign it to myGradeBook (variable??)
+        GradeBook myGradeBook = new GradeBook();
+
+        // Call myGradeBook's DisplayMessage Method
+        myGradeBook.DisplayMessage();
+
+        
+	} // End Main
+} // End GradeBookTest class
---
+++ Gradebook/GradeBook.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class GradeBook
+{
+    //Display a welcome message to the user.
+
+    public void DisplayMessage()
+    {
+        Console.WriteLine("Welcome to the Grade Book!");
+        Console.ReadLine();
+    } // end method DisplayMessage
+
+} // end class GradeBook..
